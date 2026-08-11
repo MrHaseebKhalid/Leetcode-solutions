c2b3b5abe5b14f759c09fe282bbfbbe5f8bfb273
Muhammad Haseeb Khalid <mainhaseebkhalid@gmail.com>
2026-08-11 08:56:16 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.2 MB (88.00%)

--- 0066-plus-one/solution.py
+++ 0066-plus-one/solution.py
@@ -1,30 +1,10 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        tem_list = digits[::-1]
-        n = len(tem_list)
-        l = []
-        next_check = tem_list[0]
-        index = 0
+        for i in range(len(digits)-1,-1,-1):
+            if digits[i] < 9:
+                digits[i] += 1
+                return digits
+            digits[i] = 0
+        
+        return [1] + digits
 
-        if tem_list[0] < 9:
-            tem_list = [tem_list[0] + 1] + tem_list[1:]
-
-        elif tem_list[0] == 9:
-            while next_check == 9:
-                l.append(0)
-                index += 1
-                if index <= n-1:
-                    next_check = tem_list[index]
-                elif index == n:
-                    next_check = 1
-
-            if index == n:
-                l.append(1)
-                tem_list = l
-            elif index == n-1:
-                tem_list = l + [tem_list[index]+1]
-            elif index < n-1:
-                tem_list = l + [tem_list[index]+1] + tem_list[index+1:]
-
-
-        return tem_list[::-1]
