ae4a4b1c2764a0990d4755727244f7393874254d
Muhammad Haseeb Khalid <mainhaseebkhalid@gmail.com>
2026-08-18 20:21:04 +0000

    Sync LeetCode submission Runtime - 20 ms (33.90%), Memory - 32.4 MB (22.38%)

---
+++ 0217-contains-duplicate/solution.py
@@ -0,0 +1,10 @@
+from collections import Counter
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        seen = set()
+
+        for item in nums:
+            if item in seen:
+                return True
+            seen.add(item)
+        return False
